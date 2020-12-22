13e0be9fd5fcf6176cfcd494c80612e884e0a599
Calista Mayer <calista.a.mayer@gmail.com>
2020-12-21 20:29:03 -0800

    create login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,33 @@ router.post('/', (req, res) => {
         });
 });
 
+// POST /api/users/login
+router.post('/login', (req, res) => {
+ // expects {email: 'c@li.com', password: '56}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // res.json({ user: dbUserData });
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Calista', email: 'hi@hi.com', password: 'fluff56'}
