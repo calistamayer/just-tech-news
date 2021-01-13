9e0439baa2567b44b4a3a562b43051b244c9b124
Calista Mayer <calista.a.mayer@gmail.com>
2021-01-12 16:44:48 -0800

    add comments to GET all posts route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     Post.findAll({
         // Query configuration
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -14,6 +15,14 @@ router.get('/', (req, res) => {
             [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
